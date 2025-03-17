e73689f1e2bf4cead56889c34263bf51547f143a
Oskar B. Kaare <oskar.kaare@gmail.com>
2025-03-17 17:17:44 +0100

    offline farming

--- Assets/Scripts/PlayerSounds.cs
+++ Assets/Scripts/PlayerSounds.cs
@@ -8,11 +8,29 @@ public class PlayerSounds : MonoBehaviour
     public float steprate = 0.2f;
     public float requirement = 0.1f;
 
+    public float breathDelay = 5;
+    public float time;
     private void Start()
     {
         playerMovement = GetComponent<PlayerMovement>();
         StartCoroutine(Footsteps());
+     
     }
+
+    void Update()
+    {
+        // Oskar made this, dont hate him for it, he tried his best. 
+        time += Time.deltaTime;
+        
+        if (breathDelay < time)
+        {
+            //play breath sound
+            Debug.Log("breaaath");
+            time = 0;
+        }
+
+    }
+
     IEnumerator Footsteps()
     {
         while (true)
@@ -20,14 +38,22 @@ public class PlayerSounds : MonoBehaviour
             if (playerMovement.speed > requirement)
             {
                 Debug.Log("Footstep has been stepped");
-                //Play footstep 
+                //Play footstep
             }
 
-            // dogshit, iykyk
-            yield return new WaitForSeconds(steprate * (playerMovement.speed/playerMovement.movespeed));
+            //dogshit, iykyk
+           yield return new WaitForSeconds(steprate * (playerMovement.speed / playerMovement.movespeed));
         }
     }
 
-}
+    //IEnumerable Breath()
+    //{
+    //    //play breathing sound
+    //    Debug.Log("a breath was breathed")
+    //      yield return new WaitForSeconds(steprate * (playerMovement.speed / playerMovement.movespeed));
+    //}
+    // maybe make breath connected to speed?
+}   
+
 
 
