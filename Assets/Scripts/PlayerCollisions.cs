1e602611e4d062e55c96e0aab7e64545e5c019a7
Oskar B. Kaare <oskar.kaare@gmail.com>
2025-03-17 13:49:08 +0100

    stuff n shit

--- Assets/Scripts/PlayerCollisions.cs
+++ Assets/Scripts/PlayerCollisions.cs
@@ -3,13 +3,17 @@ using UnityEngine;
 public class PlayerCollisions : MonoBehaviour
 {
     public LayerMask wallLayer;
+    private float radius = 0.5f;
+    private float offset = 0.1f;
 
-    private void OnCollisionEnter(Collision collision)
+    
+    private void OnTriggerEnter(Collider hit)
     {
-        if (collision.gameObject.layer == wallLayer)
+        if (hit.gameObject.layer == LayerMask.NameToLayer("Wall"))
         {
             Debug.Log("Player hit a wall!");
             //play Wall collision sound with Wwise ;) 
         }
     }
+   
 }
