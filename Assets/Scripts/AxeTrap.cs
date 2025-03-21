c9298d61f76e5ea7ff5f6f8229567964372d4c16
Oskar B. Kaare <oskar.kaare@gmail.com>
2025-03-21 13:33:02 +0100

    small axe fix (Average size

--- Assets/Scripts/AxeTrap.cs
+++ Assets/Scripts/AxeTrap.cs
@@ -5,10 +5,25 @@ public class AxeTrap : MonoBehaviour
 
     public int axeTrapDamage = 50;
     private BoxCollider bc;
+    public bool turbo = false;
+    public Animator anim;
     void Start()
     {
         bc = GetComponent<BoxCollider>();
+        anim = GetComponentInParent<Animator>();
+        TurboMode(turbo);
+    }
 
+    public void TurboMode(bool turbo)
+    {
+        if (turbo)
+        {
+            anim.SetBool("Turbo", true);
+        }
+        else
+        {
+            anim.SetBool("Turbo", false);
+        }
     }
 
     private void OnTriggerEnter(Collider other)
