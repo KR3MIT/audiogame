35a290a37b5801415272cf0da5ef58138f335253
William Abildtrup <william@abildtrup.eu>
2025-03-25 14:21:17 +0100

    Update TriggerRockPuzzle.cs

--- Assets/Scripts/Rock Puzzle/TriggerRockPuzzle.cs
+++ Assets/Scripts/Rock Puzzle/TriggerRockPuzzle.cs
@@ -2,22 +2,33 @@ using UnityEngine;
 
 public class TriggerRockPuzzle : MonoBehaviour
 {
-
+[SerializeField] private float currentForce;
+[SerializeField] private float forceMultiplier = 1.5f;
+[SerializeField] private bool setCustomForce = false;
+[SerializeField] private float customForce = 20f;
     private void OnTriggerEnter(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //other.GetComponent<PlayerThrow>().TriggerRock();
+            currentForce = other.GetComponent<PlayerThrow>().force;
+            if (setCustomForce)
+            {
+                other.GetComponent<PlayerThrow>().force = customForce;
+            }
+            else
+            {
+                other.GetComponent<PlayerThrow>().force = currentForce * forceMultiplier;
+            }
+            
         }
        
-        
     }
 
     private void OnTriggerExit(Collider other)
     {
         if (other.CompareTag("Player"))
         {
-            //other.GetComponent<PlayerThrow>().UnTriggerRock();
+            other.GetComponent<PlayerThrow>().force = currentForce;
         }
         
     }
