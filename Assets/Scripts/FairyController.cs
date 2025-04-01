aeaa05e60af6664d3541f7878a11d89c5cc00325
Victor Andersen <akaliasthegamer@gmail.com>
2025-04-01 14:41:59 +0200

    fairy and splines in main scene 🔥
    hell yeah!

--- Assets/Scripts/FairyController.cs
+++ Assets/Scripts/FairyController.cs
@@ -8,23 +8,43 @@ public class FairyController : MonoBehaviour
 {
     public SplineAnimate splineAnimate;
     public List<SplineContainer> splines;
+    int currentSpline = 0;
+    bool hasPlayedOnSpline = false;
 
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
         SplineAnimate splineAnimate = GetComponent<SplineAnimate>();
-       
-        splineAnimate.Container = splines[1];
-        splineAnimate.Play();
-        
-        
+        splineAnimate.Container = splines[currentSpline];
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (splineAnimate.IsPlaying == false && hasPlayedOnSpline == true)
+        {
+            splineAnimate.Pause();
+            
+            splineAnimate.Container = splines[currentSpline];
+            
+        }
     }
+
+    void OnTriggerEnter(Collider other)
+    {
+        Debug.Log(splineAnimate.IsPlaying);
+        if (other.gameObject.tag == "Player" && splineAnimate.IsPlaying != true)
+        { 
+            splineAnimate.Play();
+            currentSpline++;
+            hasPlayedOnSpline = true;
+            Debug.Log("Triggered");
+
+        }
+
+    }
+
+
 }
 
