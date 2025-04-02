e07f07533e520680a18adb8c229e00c4f5b071ed
Mathias Kofoed Hansen <mkha23@student.aau.dk>
2025-04-02 10:18:04 +0200

    Made fairy event tihi

--- Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
+++ Assets/StreamingAssets/Audio/GeneratedSoundBanks/Wwise_IDs.h
@@ -14,6 +14,7 @@ namespace AK
     namespace EVENTS
     {
         static const AkUniqueID AXESWINGEVENT = 3270970525U;
+        static const AkUniqueID FAIRYEVENT = 3885387924U;
         static const AkUniqueID FOOTSTEPEVENT = 3511205013U;
         static const AkUniqueID PLAY_GLOWSWITCHCONTROLLER = 3599396917U;
         static const AkUniqueID PLAY_WALLIMPACTANDRUB = 1324720394U;
