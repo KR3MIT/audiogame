8ed57fd2b7251a40a015f16666841a4bab55afa4
Oskar B. Kaare <oskar.kaare@gmail.com>
2025-04-02 12:51:48 +0200

    4wed5tgvybnijm

--- Assets/EdgeRockSounds.cs
+++ Assets/EdgeRockSounds.cs
@@ -1,16 +1,33 @@
+using System.Collections;
 using UnityEngine;
 
 public class EdgeRockSounds : MonoBehaviour
 {
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-        
-    }
+    public AK.Wwise.Event soundEvent;
+    public BoxCollider bc;
 
-    // Update is called once per frame
-    void Update()
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+
+    private void Start()
     {
+        bc = GetComponent<BoxCollider>();
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if(other.TryGetComponent(out PlayerBehavior player))
+        {
+            Debug.Log("fall rocks r falling");
+            soundEvent.Post(gameObject);
+            StartCoroutine(SoundReset());
+        }
         
     }
+    IEnumerator SoundReset()
+    {
+        bc.enabled = false;
+        yield return new WaitForSeconds(10f);
+        bc.enabled = true;
+    }
+    
 }
